a03a34390a0c5405c66e5f07b233789eb235c9ed
adelakim <diddnjs02@likelion.org>
2020-01-21 17:12:41 +0900

    jvm

--- banana/mango/transformdata.py
+++ banana/mango/transformdata.py
@@ -20,7 +20,9 @@ class transformData:
             questions = Question.objects.all().filter(userId=self.user)
             for question in questions:
                 total = total + question.answer
-            
+            userTotal = User_Chat.objects.get(user=self.user)
+            userTotal.total = total
+            userTotal.save()
             data = {
                 "version": "2.0",
                 "template": {
